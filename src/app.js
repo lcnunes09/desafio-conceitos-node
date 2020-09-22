2f21f0e854746255c91c6ae63afe6fa68b195be9
Luciana Nunes <lcnunes09@gmail.com>
2020-09-21 22:42:10 -0300

    Delete repository

--- src/app.js
+++ src/app.js
@@ -26,11 +26,21 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
